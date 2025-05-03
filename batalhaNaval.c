79973edcf89d3bd23622c3baeafc06a92d310d79
Luiz Miguel <140824165+LuizMiguel-k1ngm@users.noreply.github.com>
2025-05-03 09:59:30 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -4,7 +4,7 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
 // Siga os comentários para implementar cada parte do desafio.
 
-int main() {
+
    int main() {
 	
 	int tabuleiro[10][10];
