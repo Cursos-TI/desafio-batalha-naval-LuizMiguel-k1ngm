b2e44f139309bb25a52612915a222d3a26142d1f
Luiz Miguel <140824165+LuizMiguel-k1ngm@users.noreply.github.com>
2025-05-04 18:20:16 -0300

    Update batalhaNaval.c

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,43 +1,77 @@
 #include <stdio.h>
 
-// Desafio Batalha Naval - MateCheck
-// Este código inicial serve como base para o desenvolvimento do sistema de Batalha Naval.
-// Siga os comentários para implementar cada parte do desafio.
-
-#include <stdio.h>
-
  int main() {
-    char matriz[10][10];  // matriz 10x10 de caracteres
+     char matriz[10][10];
 
-    // Inicializar a matriz com '0'
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
+     for (int i = 0; i < 10;  i++){
+        for (int j =0;  j < 10;  j++){
             matriz[i][j] = '0';
         }
+     }
+
+
+   // colocar os navios na vertical e horizontal
+
+matriz[1][2] = '3';
+matriz[2][2] = '3';
+matriz[3][2] = '3';
+
+matriz[6][5] ='3';
+matriz[6][6] ='3';
+matriz[6][7] ='3';
+
+// cone
+matriz [6][4]='1';
+
+for(int j = 3; j <=5; j++){
+    matriz[7][j] = '1';
+}
+
+for(int j = 2; j <=6; j++){
+    matriz[8][j] = '1';
+}
+
+
+// octaedro
+
+matriz[0][6] = '1';
+matriz[2][6] = '1';
+
+for(int j = 5; j<=7; j++){
+    matriz[1][j] = '1';
+}
+
+
+
+//cruz
+
+matriz[3][3] = '1';
+matriz[5][3] = '1';
+
+for(int j = 1; j<=5; j++){
+    matriz[4][j] = '1';
+}
+
+
+
+
+
+
+
+printf("Matriz com navios\n");
+
+for(int i = 0; i <10; i++){
+    for(int j = 0; j<10; j++){
+        printf("%c ", matriz[i][j]);
     }
 
-    // Colocar navio vertical com '3' em (1,2), (2,2), (3,2)
-    matriz[1][2] = '3';
-    matriz[2][2] = '3';
-    matriz[3][2] = '3';
+    printf("\n");
+}
 
-    // Colocar navio horizontal com '3' em (6,5), (6,6), (6,7)
-    matriz[6][5] = '3';
-    matriz[6][6] = '3';
-    matriz[6][7] = '3';
 
-    // Exibir a matriz
-    printf("Matriz com navios:\n");
-    for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++) {
-            printf(" %c ", matriz[i][j]);
-        }
-        printf("\n");
-    }
 
     return 0;
 }
-	
-	
+
 	
 
